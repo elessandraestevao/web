8f96dd4f594345be11f237fb7675f893be5817e1
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-06-30 13:27:24 -0300

    Capitulo 3 - Controlando requisicoes e a listagem de produtos

---
+++ ControleDeEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            IList<CategoriaDoProduto> categorias = new CategoriasDAO().Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+    }
+}
