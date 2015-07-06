8ad69144170076a0be67c1ee8c999da4105132bc
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-07-05 22:53:34 -0300

    Capitulo 10 - Sessoes para autenticar usuarios

---
+++ ControleDeEstoque/CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using CaelumEstoque.Models;
+using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string nome, string senha)
+        {
+            Usuario usuario = new UsuariosDAO().Busca(nome, senha);
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
